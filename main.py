043bf4bc806da392d952ad2636432dac2c8e9ea4
Mustafa <mustafamcm9@gmail.com>
2023-04-29 14:09:22 +0300

    add analyzer   Add print statements to reflect the analyze of the book

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 # count words in text
-def count_word(text):
+def count_words(text):
     l = text.split()
     return len(l)
 # count letters
 def count_letters(text):
     output = {}
     for s in text:
-        # Include if character is letter
+        # make characters lowercase
+        str = s.lower()
+        # include only if character is letter
         if(s.isalpha()):
-            str = s.lower()
-        if str in output:
-            output[str] += 1
-        else:
-            output[str] = 1
+            if str in output:
+                # character is found, add +1 to it's count
+                output[str] += 1
+            else:
+                # character is not found, make it's count 1
+                output[str] = 1
     # sort the output on descending order
     sorted_output = sorted(output.items(), key=lambda x: x[1], reverse=True)
     # convert sorted output to dictionary again
     converted = dict(sorted_output)
     return converted
         
-
-with open("books/frankenstein.txt") as file:
+path = "books/frankenstein.txt"
+# open file
+with open(path) as file:
+    # read the file
     file_contents = file.read()
-    print(count_letters(file_contents))
+    print(f"--- Begin report of {path} ---")
+    # print word count of the file
+    print(f"{count_words(file_contents)} words found in the document\n")
+    letter_dict = count_letters(file_contents)
+    # print letter count of the file
+    for char in letter_dict:
+        print(f"The '{char}' character was found {letter_dict[char]} times")
